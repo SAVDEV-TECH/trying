34f659b53fc2a8ed5f1f133aedd1a9642ad45705
Savdev <savde388@gmail.com>
2024-07-25 17:39:42 -0700

    yea

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,7 +4,7 @@ import HeaderContent from './HeaderContent'
 
 function Header() {
   return (
-    <div className='w-full bg-cover bg-center relative h-[70vh] ' style={{ backgroundImage:"url(/logo192.png)"}}> 
+    <div className='w-full bg-cover bg-center relative h-[70vh] ' style={{ backgroundImage:"url(/image.jfif)"}}> 
     <div className='  h-screen bg-black absolute top-0 bottom-0 right-0 left-0 z-[1] opacity-50'></div>
       <Navbar></Navbar>
       <HeaderContent ></HeaderContent>
